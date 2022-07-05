8c60f7b1354764f598c5d78bb8083efc53d409b2
Justin Lepine <lepine.justin@yahoo.com>
2022-07-05 16:20:45 -0400

    channel search

--- src/components/ChannelSearch.jsx
+++ src/components/ChannelSearch.jsx
@@ -1,43 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import { getChannel, useChatContext } from 'stream-chat-react';
-import { ResultsDropdown } from './';
+import { useChatContext } from 'stream-chat-react';
 
-import { SearchIcon } from '../assets'
+import { ResultsDropdown } from './'
+import { SearchIcon } from '../assets';
 
 const ChannelSearch = ({ setToggleContainer }) => {
     const { client, setActiveChannel } = useChatContext();
     const [query, setQuery] = useState('');
     const [loading, setLoading] = useState(false);
-    const [teamChannels, setTeamChannels] = useState([]);
-    const [directChannels, setDirectChannels] = useState([]);
+    const [teamChannels, setTeamChannels] = useState([])
+    const [directChannels, setDirectChannels] = useState([])
 
     useEffect(() => {
         if(!query) {
-            setTeamChannels([])
-            setDirectChannels([])
+            setTeamChannels([]);
+            setDirectChannels([]);
         }
-
     }, [query])
-    
 
     const getChannels = async (text) => {
         try {
             const channelResponse = client.queryChannels({
-                type: 'team',
-                name: { $autocomplete: text },
-                members: {$in: [client.userID]}
+                type: 'team', 
+                name: { $autocomplete: text }, 
+                members: { $in: [client.userID]}
             });
             const userResponse = client.queryUsers({
                 id: { $ne: client.userID },
                 name: { $autocomplete: text }
-            });
+            })
 
-            const [channels, { users }] = await Promise.all({channelResponse, userResponse});
+            const [channels, { users }] = await Promise.all([channelResponse, userResponse]);
 
-            if(channelResponse.length) setTeamChannels(channels);
+            if(channels.length) setTeamChannels(channels);
             if(users.length) setDirectChannels(users);
-
-        } catch(error) {
+        } catch (error) {
             setQuery('')
         }
     }
@@ -55,32 +52,32 @@ const ChannelSearch = ({ setToggleContainer }) => {
         setActiveChannel(channel);
     }
 
-  return (
-    <div className='channel-search__container'>
-        <div className='channel-search__input__wrapper'>
-            <div className='channel-search__input__icon'>
-                <SearchIcon />
+    return (
+        <div className="channel-search__container">
+            <div className="channel-search__input__wrapper">
+                <div className="channel-serach__input__icon">
+                    <SearchIcon />
+                </div>
+                <input 
+                    className="channel-search__input__text" 
+                    placeholder="Search" 
+                    type="text" 
+                    value={query}  
+                    onChange={onSearch}
+                />
             </div>
-            <input 
-                className='channel-search__input__text'
-                placeholder='Search'
-                type='text'
-                value={query}
-                onChange={onSearch}
-            />
+            { query && (
+                <ResultsDropdown 
+                    teamChannels={teamChannels}
+                    directChannels={directChannels}
+                    loading={loading}
+                    setChannel={setChannel}
+                    setQuery={setQuery}
+                    setToggleContainer={setToggleContainer}
+                />
+            )}
         </div>
-        { query && (
-            <ResultsDropdown
-                teamChannels={teamChannels}
-                directChannels={directChannels}
-                loading={loading}
-                setChannel={setChannel}
-                setQuery={setQuery}
-                setToggleContainer={setToggleContainer}
-            />
-        )}
-    </div>
-  )
+    )
 }
 
 export default ChannelSearch
